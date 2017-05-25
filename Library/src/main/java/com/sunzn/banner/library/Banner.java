487aac89fee539da9cf09ff36032c4798749c75e
sunzn <sunzn.cn@gmail.com>
2017-05-25 10:43:00 +0800

    Fix playBanner() method

--- Library/src/main/java/com/sunzn/banner/library/Banner.java
+++ Library/src/main/java/com/sunzn/banner/library/Banner.java
@@ -282,6 +282,7 @@ public class Banner extends FrameLayout {
 
     public void playBanner() {
         if (mHandler != null && !isPlaying && mBannerAdapter.getItemCount() > 1) {
+            mHandler.removeCallbacksAndMessages(null);
             mHandler.postDelayed(mBannerTask, mInterval);
             isPlaying = true;
         }
